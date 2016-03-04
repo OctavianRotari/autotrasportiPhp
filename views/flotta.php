8674e58dba682a9cc9a3de7060d7c9136d911401
Octavian Rotari <octavianrotari@gmail.com>
2016-03-04 17:16:23 +0100

    sent to host

--- views/flotta.php
+++ views/flotta.php
@@ -7,10 +7,39 @@
 	    <hr class="featurette-divider">
 		  <div class="featurette-heading container">
 			<div class="col-md-5 col-md-push-7">
-			      <img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" src="./../img/zimbet2-1-1.jpg" alt="camion">
+			      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+				    <ol class="carousel-indicators">
+					  <li data-target="#myCarousel" data-slide-to="0" class="active">
+					  <li data-target="#myCarousel" data-slide-to="1">
+					  <li data-target="#myCarousel" data-slide-to="2">
+					  <li data-target="#myCarousel" data-slide-to="3">
+				    </ol>
+				    <div class="carousel-inner" role="listbox">
+					  <div class="item active">
+						<img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" src="./../img/zimbet2-1-1.jpg" alt="camion">
+					  </div>
+					  <div class="item">
+						<img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" src="./../img/zimbet2-1-1.jpg" alt="camion">
+					  </div>
+					  <div class="item">
+						<img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" src="./../img/zimbet2-1-1.jpg" alt="camion">
+					  </div>
+					  <div class="item">
+						<img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" src="./../img/zimbet2-1-1.jpg" alt="camion">
+					  </div>
+				    </div>
+				    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+					  <span class"glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					  <span class="sr-only">Previous</span>
+				    </a>
+				    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+					  <span class"glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					  <span class="sr-only">Next</span>
+				    </a>
+			      </div>
 			</div>
-			<div class="col-md-7 col-md-pull-5">
-<div class="my-container">
+		  <div class="col-md-7 col-md-pull-5">
+			<div class="my-container">
 			      <h2 class="featurette-heading">Motrici</h2>
 			      <p class="lead">
 				    Il nostro Parco Mezzi, viene normalmente controllato e periodicamente rinnovato
@@ -26,7 +55,36 @@
 	    <hr class="featurette-divider">
 		  <div class="featurette-heading container">
 			<div class="col-md-5 col-md-push-7">
-			      <img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/auto" src="./../img/zimbet2-1-1.jpg" alt="camion">
+			      <div id="myCarousel2" class="carousel slide" data-ride="carousel">
+			      <ol class="carousel-indicators">
+				    <li data-target="#myCarousel" data-slide-to="0" class="active">
+				    <li data-target="#myCarousel" data-slide-to="1">
+				    <li data-target="#myCarousel" data-slide-to="2">
+				    <li data-target="#myCarousel" data-slide-to="3">
+			      </ol>
+			      <div class="carousel-inner" role="listbox">
+				    <div class="item active">
+					  <img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" src="./../img/zimbet2-1-1.jpg" alt="camion">
+				    </div>
+				    <div class="item">
+					  <img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" src="./../img/zimbet2-1-1.jpg" alt="camion">
+				    </div>
+				    <div class="item">
+					  <img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" src="./../img/zimbet2-1-1.jpg" alt="camion">
+				    </div>
+				    <div class="item">
+					  <img class="featurette-image img-responsive center-block img-rounded" data-src="holder.js/500x500/auto" src="./../img/zimbet2-1-1.jpg" alt="camion">
+				    </div>
+			      </div>
+			      <a class="left carousel-control" href="#myCarousel2" role="button" data-slide="prev">
+				    <span class"glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+			      </a>
+			      <a class="right carousel-control" href="#myCarousel2" role="button" data-slide="next">
+				    <span class"glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+			      </a>
+			      </div>
 			</div>
 			<div class="col-md-7 col-md-pull-5">
 	    <div class="my-container">
